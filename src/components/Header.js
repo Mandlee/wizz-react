ac353544d8c1f68bcffaf408bd444a6206e7d7ff
Bence Mándli <mandli.bence@gmail.com>
2018-10-11 11:17:04 +0200

    Improve header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,18 @@ const Header = props => (
         <Link to="/">
             <img src={logo} className="header__logo" alt="logo" />
         </Link>
-        <h1 className="header__title">Header</h1>
         <div className="header__flight-route flight-route">
             <span className="flight-route__title">Leaving from</span>
             <span className="flight-route__origin-full">Budapest</span> <span className="flight-route__origin-short">BUD</span>
             <i className="icon icon__return-arrow flight-route__icon"/>
             <span className="flight-route__origin-full">Barcelona Lorem ipsum</span> <span className="flight-route__origin-short">BCN</span>
+        </div>
+        <div className="header__flight-route flight-route flight-route--flight-passengers">
+            <span className="flight-route__title">PASSENGERS</span>
+            <span className="flight-route__origin-full">1</span>
+            <i className="icon icon__person flight-route--flight-passengers__icon-person"/>
+            <span className="flight-route__origin-full">3</span>
+            <i className="icon icon__person icon__person--child flight-route--flight-passengers__icon-child"/>
         </div>
     </header>
 );
